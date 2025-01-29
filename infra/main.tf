646106a805446b06cf3eb6dad5bcb1b4cbf4eab0
Mike Beaumont <mjboamail@gmail.com>
2025-01-29 20:32:13 +0100

    Revert "chore(deps): update terraform cloudflare to v5 (#51)"
    This reverts commit 824e32d5682da60924e2bd2dfdb74e8022b8eb39.

--- infra/main.tf
+++ infra/main.tf
@@ -26,7 +26,7 @@ terraform {
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 5.0"
+      version = "~> 4.0"
     }
   }
 }
--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 5.0"
+      version = "~> 4.0"
     }
   }
 }
