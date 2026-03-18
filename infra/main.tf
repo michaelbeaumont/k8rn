1497fe0fe6b61ddc6236aee1846ef457aa817d35
Mike Beaumont <mjboamail@gmail.com>
2026-03-18 20:03:55 +0100

    feat(infra): new provider version + set explicit images to avoid upgrade failure

--- infra/main.tf
+++ infra/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     talos = {
       source  = "siderolabs/talos"
-      version = "0.10.1"
+      version = "0.11.0-beta.1"
     }
     tailscale = {
       source  = "tailscale/tailscale"
